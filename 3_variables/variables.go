8f14497c105cb56f98f9563796962efa28760d28
Prateek Kumar <prateekkumar72007@gmail.com>
2025-05-12 00:28:10 +0530

    Refactor variable declarations and comments for clarity in variables.go

--- 3_variables/variables.go
+++ 3_variables/variables.go
@@ -1,5 +1,17 @@
 package main
 import "fmt"
-func main() {
 
+
+func main() {
+	//  we need to declare a variable type at time of deceleration if we do not assign a value to it at the time of deceleration
+var name string = "Prateek" // string
+// var name = "John Doe" // string
+var age int = 30 // int
+
+// shorthand syntax
+
+// name := "John Doe" // string 
+
+fmt.Println("Name:", name)
+fmt.Println("Age:", age)
 }
