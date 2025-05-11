48207dfae53d2478706e012ab46732f092cef443
Prateek Kumar <prateekkumar72007@gmail.com>
2025-05-11 21:51:02 +0530

    Add initial Go files for Hello World, simple values, and variables examples

---
+++ 2_simpleValues/main.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+func main() {
+// simple values
+// int
+fmt.Println(1) 
+fmt.Println(1+1)
+// string
+fmt.Println("Hello, Golang!")
+// boolean
+fmt.Println(true)
+fmt.Println(false)
+// float
+fmt.Println(1.5)
+fmt.Println(1.5 + 1.5)
+// complex
+fmt.Println(1 + 2i)
+fmt.Println(1 + 2i + 3.5)
+}
